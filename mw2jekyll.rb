e449ce67ddeead03fd9b77c3634eca1814ce8961
David Jones <david.jones@fmail.co.uk>
2014-05-05 15:24:49 +0100

    Move copyright block into a variable

--- mw2jekyll.rb
+++ mw2jekyll.rb
@@ -3,23 +3,22 @@
 
 ## mw2jekyll.rb --- MediaWiki database -> Jekyll Git repository
 
-# Author::    David Jones (mailto: david@djones.eu)
-# Copyright:: Copyright (C) 2014 David Jones
-# License::   GNU GPLv3+
+COPYRIGHT = <<END
+Copyright (C) 2014 David Jones <david at djones.eu>
 
-# This script is free software: you can redistribute it and/or modify it
-# under the terms of the GNU General Public License as published by the
-# Free Software Foundation, either version 3 of the License, or (at your
-# option) any later version.
+This script is free software: you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation, either version 3 of the License, or (at your
+option) any later version.
 
-# This script is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# General Public License for more details.
+This script is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
 
-# You should have received a copy of the GNU General Public License
-# along with this script.  If not, see <http://www.gnu.org/licenses/>.
-VERSION = '0.0.0.beta'
+You should have received a copy of the GNU General Public License
+along with this script.  If not, see <http://www.gnu.org/licenses/>.
+END
 
 ## Commentary:
 
